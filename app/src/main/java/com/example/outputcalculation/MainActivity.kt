77647ee5f5059304e95f40b119faf931c6f4b626
novatoka <oresama_sanka@yahoo.co.jp>
2019-11-04 22:05:28 +0900

    画面デザイン作成完了

--- app/src/main/java/com/example/outputcalculation/MainActivity.kt
+++ app/src/main/java/com/example/outputcalculation/MainActivity.kt
@@ -1,6 +1,7 @@
+package com.example.outputcalculation
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.outputcalculation.R
 
 class MainActivity : AppCompatActivity() {
 
