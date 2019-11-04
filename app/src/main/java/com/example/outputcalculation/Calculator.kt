72bd0a6f5e62e5b89dd309d35a53c76164dee0f1
novatoka <oresama_sanka@yahoo.co.jp>
2019-11-04 22:35:06 +0900

    計算を行うメソッドの追加

---
+++ app/src/main/java/com/example/outputcalculation/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.outputcalculation
+
+class Calculator {
+
+    //計算を行うメソッド
+    fun execCalculation{
+        val result = 0
+
+        return result
+
+    }
+}
--- app/src/main/java/com/example/outputcalculation/MainActivity.kt
+++ app/src/main/java/com/example/outputcalculation/MainActivity.kt
@@ -9,4 +9,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Sendボタン押下時
+    @SuppressLint("SetTextI18n")
+    fun tapCalculationButton(view: View) {
+        // 計算処理を行う
+        val num1Str = (findViewById<EditText>(R.id.num1)).text.toString()
+        val num2Str = (findViewById<EditText>(R.id.num2)).text.toString()
+        val symbolStr = (findViewById<Spinner>(R.id.spinner)).selectedItem.toString()
+        val result = Calculator().execCalculation(num1Str, num2Str, symbolStr)
+        resultTextView.text = num1Str + symbolStr + num2Str + "=" + result.toString()
+    }
 }
